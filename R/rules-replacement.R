1b33dcf1a40a91b943112abbce5fc4d8a7c83ce1
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-30 15:56:29 +0100

    don't replace comments with empty string, simply remove them

--- R/rules-replacement.R
+++ R/rules-replacement.R
@@ -16,28 +16,6 @@ resolve_semicolon <- function(pd) {
 
 remove_comments <- function(pd) {
   is_comment <- pd$token == "COMMENT"
-  if (any(is_comment) && any(pd$lag_newlines[is_comment]) > 0L) {
-    lag_newlines <- reduce(rev(which(is_comment)),
-      function(x, index) add_to_element_above(x, index),
-      .init = pd$lag_newlines
-    )
-    pd$lag_newlines <- lag_newlines
-    pd <- pd[!is_comment,]
-
-  }
+  pd <- pd[!is_comment,]
   pd
-}
-
-#' Add elements in a vector to the elment before it
-#'
-#' @param x A vector
-#' @param index A scalar indicating from which elements in the vector should
-#'   be added to its left neighbour.
-#' @examples
-#' add_to_element_above(c(1, 2, 3), 2)
-add_to_element_above <- function(x, index) {
-  if (x[index] > 0L) {
-    x[index - 1L] <- sum(x[c(index - 1L, index)])
-  }
-  x
 }
