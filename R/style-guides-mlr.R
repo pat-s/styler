ab302b01188e092f9bc5c0aad73c98fd1daf5ee1
lorenzwalthert <lorenz.walthert@icloud.com>
2018-06-13 13:09:27 +0200

    simply copy over from tidyverse style.

---
+++ R/style-guides-mlr.R
@@ -0,0 +1,147 @@
+
+#' The mlr style
+#'
+#' Style code according to the mlr style guide.
+#' @inheritParams tidyverse_style
+#' @details The following options for `scope` are available.
+#'
+#' * "none": Performs no transformation at all.
+#' * "spaces": Manipulates spacing between token on the same line.
+#' * "indention": In addition to "spaces", this option also manipulates the
+#'   indention level.
+#' * "line_breaks": In addition to "indention", this option also manipulates
+#'   line breaks.
+#' * "tokens": In addition to "line_breaks", this option also manipulates
+#'   tokens.
+#'
+#' As it becomes clear from this description, more invasive operations can only
+#' be performed if all less invasive operations are performed too.
+#' @family obtain transformers
+#' @family style_guides
+#' @examples
+#' style_text("call( 1)", style = mlr_style, scope = "spaces")
+#' style_text("call( 1)", transformers = mlr_style(strict = TRUE))
+#' @importFrom purrr partial
+#' @export
+mlr_style <- function(scope = "tokens",
+                            strict = TRUE,
+                            indent_by = 2,
+                            start_comments_with_one_space = FALSE,
+                            reindention = tidyverse_reindention(),
+                            math_token_spacing = tidyverse_math_token_spacing()) {
+  scope <- character_to_ordered(
+    scope,
+    c("none", "spaces", "indention", "line_breaks", "tokens")
+  )
+
+  space_manipulators <- if (scope >= "spaces") {
+    lst(
+      indent_braces = partial(indent_braces, indent_by = indent_by),
+      unindent_fun_dec,
+      indent_op = partial(indent_op, indent_by = indent_by),
+      indent_eq_sub = partial(indent_eq_sub, indent_by = indent_by),
+      indent_without_paren = partial(indent_without_paren,
+                                     indent_by = indent_by
+      ),
+      fix_quotes,
+      remove_space_before_closing_paren,
+      remove_space_before_opening_paren = if (strict) remove_space_before_opening_paren,
+      add_space_after_for_if_while,
+      add_space_before_brace,
+      remove_space_before_comma,
+      style_space_around_math_token = partial(
+        style_space_around_math_token, strict,
+        math_token_spacing$zero,
+        math_token_spacing$one
+      ),
+      style_space_around_tilde = partial(
+        style_space_around_tilde, strict = strict
+      ),
+      spacing_around_op = if (strict) {
+        set_space_around_op
+      }else {
+        add_space_around_op
+      },
+      spacing_around_comma = if (strict) {
+        set_space_after_comma
+      } else {
+        add_space_after_comma
+      },
+      remove_space_after_opening_paren,
+      remove_space_after_excl,
+      set_space_after_bang_bang,
+      remove_space_before_dollar,
+      remove_space_after_fun_dec,
+      remove_space_around_colons,
+      start_comments_with_space = partial(start_comments_with_space,
+                                          force_one = start_comments_with_one_space
+      ),
+      remove_space_after_unary_pm_nested,
+      spacing_before_comments = if (strict) {
+        set_space_before_comments
+      } else {
+        add_space_before_comments
+      },
+      set_space_between_levels,
+      set_space_between_eq_sub_and_comma
+    )
+  }
+
+  use_raw_indention <- scope < "indention"
+
+  line_break_manipulators <- if (scope >= "line_breaks") {
+    lst(
+      remove_line_break_before_curly_opening,
+      remove_line_break_before_round_closing_after_curly =
+        if (strict) remove_line_break_before_round_closing_after_curly,
+      remove_line_break_before_round_closing_fun_dec =
+        if (strict) remove_line_break_before_round_closing_fun_dec,
+      style_line_break_around_curly = partial(style_line_break_around_curly,
+                                              strict
+      ),
+      set_line_break_after_opening_if_call_is_multi_line = if (strict)
+        partial(
+          set_line_break_after_opening_if_call_is_multi_line,
+          except_token_after = "COMMENT",
+          except_text_before = c("switch", "ifelse", "if_else")
+        ),
+      set_line_break_before_closing_call = if (strict) {
+        partial(
+          set_line_break_before_closing_call, except_token_before = "COMMENT"
+        )
+      },
+      remove_line_break_in_empty_fun_call,
+      add_line_break_after_pipe
+    )
+  }
+
+  token_manipulators <- if (scope >= "tokens") {
+    lst(
+      force_assignment_op,
+      resolve_semicolon,
+      add_brackets_in_pipe,
+      remove_terminal_token_before_and_after,
+      wrap_if_else_multi_line_in_curly =
+        if (strict) wrap_if_else_multi_line_in_curly
+    )
+  }
+
+
+  indention_modifier <-
+    lst(
+      update_indention_ref_fun_dec =
+        if (scope >= "indention") update_indention_ref_fun_dec
+    )
+
+  create_style_guide(
+    # transformer functions
+    initialize        = default_style_guide_attributes,
+    line_break        = line_break_manipulators,
+    space             = space_manipulators,
+    token             = token_manipulators,
+    indention         = indention_modifier,
+    # transformer options
+    use_raw_indention = use_raw_indention,
+    reindention       = reindention
+  )
+}
