760160a0d5a373c41a8547c9fe322a19faf52e2e
lorenzwalthert <lorenz.walthert@icloud.com>
2019-01-17 16:09:55 +0100

    fix test

--- tests/testthat/test-mlr-style.R
+++ tests/testthat/test-mlr-style.R
@@ -1,5 +1,8 @@
 context("test-mlr-style")
 
 test_that("line break for multi-line function declaration", {
-  test_collection("mlr-style", "line-break")
+  test_collection("mlr-style", "line-break",
+    transformer = style_text,
+    style = mlr_style
+  )
 })
