e582b491c149bc283f42370d4dd0750e9b58b0d3
Patrick Schratz <patrick.schratz@gmail.com>
2019-05-09 16:07:17 +0200

    force line_break_before_round_closing_after_curly

--- R/style-guides-mlr.R
+++ R/style-guides-mlr.R
@@ -102,6 +102,7 @@ mlr_style <- function(scope = "tokens",
       ),
       remove_line_break_in_empty_fun_call,
       add_line_break_after_pipe,
+      add_line_break_before_round_closing_after_curly(),
       # should be last because it depends on other line breaks via n_lines()
       partial(set_line_break_after_fun_dec_header,
               min_lines_for_break = min_lines_for_break
